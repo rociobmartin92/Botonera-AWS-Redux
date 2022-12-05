247b36c90a8be4cebd97edda3937ef832bf73a11
rocio_martin98 <rocio_martin98@hotmail.com>
2022-12-05 13:14:01 -0300

    stack created

--- botonera/App.tsx
+++ botonera/App.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {NativeBaseProvider, Text} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 import {theme} from './src/theme';
 import {SafeAreaView} from 'react-native';
+import RootNavigation from './src/stack';
 
 const App = () => {
   return (
     <NativeBaseProvider theme={theme}>
       <SafeAreaView>
-        <Text color="primario.medium">sdfsd</Text>
+        <RootNavigation />
       </SafeAreaView>
     </NativeBaseProvider>
   );
