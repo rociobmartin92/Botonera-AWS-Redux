d6b7fed3f8e82c762a2dc4c217f187b2f49dfcfc
rocio_martin98 <rocio_martin98@hotmail.com>
2022-12-06 11:21:40 -0300

    change splash and styles on welcome

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,11 +1,11 @@
-import {SafeAreaView} from 'react-native';
-import React from 'react';
-import {Box, Button, Image, Text, VStack} from 'native-base';
+import { SafeAreaView } from "react-native";
+import React from "react";
+import { Box, Button, Image, Text, VStack } from "native-base";
 
-import {useAuthenticator} from '@aws-amplify/ui-react-native';
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
 
 function SignOutButton() {
-  const {signOut} = useAuthenticator();
+  const { signOut } = useAuthenticator();
   return (
     <>
       <Button onPress={signOut}>Salir de mi cuenta</Button>
@@ -13,16 +13,16 @@ function SignOutButton() {
   );
 }
 
-const img = require('../assets/boots.jpeg');
+const img = require("../assets/boots.jpeg");
 // const donut = require('../assets/donut.png');
 
-const Welcome = ({navigation}: any) => {
+const Welcome = ({ navigation }: any) => {
   return (
     <SafeAreaView>
       <Box mt={5} mb={10} alignItems="center">
-        <Text fontSize={24} color="#d45f83" mb={1}>
+        {/* <Text fontSize={20} color="#d45f83" mb={1}>
           ¡Te damos la bienvenida!
-        </Text>
+        </Text> */}
         {/* <Image
           borderRadius={10}
           alt="imagen dona"
@@ -31,7 +31,7 @@ const Welcome = ({navigation}: any) => {
           height={7}
         /> */}
       </Box>
-      <Box alignItems="center" mt={10}>
+      <Box alignItems="center"s>
         <Image
           borderRadius={10}
           alt="imagen logo"
@@ -42,25 +42,27 @@ const Welcome = ({navigation}: any) => {
         <Button
           mt={6}
           mb={3}
-          _text={{color: 'bot.green', fontSize: 17}}
-          _pressed={{backgroundColor: 'transparent'}}
+          _text={{ color: "bot.green", fontSize: 16 }}
+          _pressed={{ backgroundColor: "transparent" }}
           borderWidth="1"
-          borderColor="bot.gold"
+          borderColor="#d45f83"
           borderRadius={7}
           width="65%"
           variant="outline"
-          onPress={() => navigation.navigate('login')}>
+          onPress={() => navigation.navigate("login")}
+        >
           Ingresar
         </Button>
         <Button
           borderRadius={7}
-          _text={{color: 'bot.green', fontSize: 17}}
-          _pressed={{backgroundColor: 'transparent'}}
+          _text={{ color: "bot.green", fontSize: 16 }}
+          _pressed={{ backgroundColor: "transparent" }}
           borderWidth="1"
-          borderColor="bot.gold"
+          borderColor="#d45f83"
           width="65%"
           variant="outline"
-          onPress={() => navigation.navigate('register')}>
+          onPress={() => navigation.navigate("register")}
+        >
           Registrarme
         </Button>
       </Box>
