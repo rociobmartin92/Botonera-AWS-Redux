7146c5944ad7a6722bd0247af20acc8712bb76b2
Diego Martin Marmol <diego.marmol@dataart.com>
2022-12-21 16:54:07 -0300

    feat: missing amplify files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3botonerastorageae6e8998": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
